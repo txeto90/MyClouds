5c0065d7ac79acbd14fe5844fd98ad0fce267df9
Jomoniz <herpessenyor@gmail.com>
2020-06-10 10:34:56 +0200

    falta: - quizz game
    diseny:
    -dicotomica
    -quizz game
    -maint activity

--- app/src/main/java/com/example/myclouds/activity/MainActivity.java
+++ app/src/main/java/com/example/myclouds/activity/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.myclouds.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.example.myclouds.R;
@@ -12,6 +13,7 @@ import androidx.appcompat.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ImageButton;
 
 public class MainActivity extends MainMenu {
 
@@ -22,7 +24,28 @@ public class MainActivity extends MainMenu {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        ImageButton ibLlista = findViewById(R.id.ibLlista);
+        ImageButton ibDico = findViewById(R.id.ibDico);
+        ImageButton ibJoc = findViewById(R.id.ibJoc);
 
+        ibLlista.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), NuvolListActivity.class));
+            }
+        });
+        ibDico.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), DicotomicaActivity.class));
+            }
+        });
+        ibJoc.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //startActivity(new Intent(getApplicationContext(), QuizzActivity.class));
+            }
+        });
     }
 
 
